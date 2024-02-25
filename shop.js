53d555b227655be16421fd869b7ddb884007152f
statkins <statkins@uiowa.edu>
2024-02-25 01:20:41 -0600

    storage button funct

--- shop.js
+++ shop.js
@@ -15,16 +15,46 @@ const shopMainButton = document.querySelector('#shopButton');
 const shopContainer = document.querySelector('#shop-container');
 const storageMainButton = document.querySelector('#invButton');
 const storageContainer = document.querySelector('#storage-container');
+const coinButton = document.querySelector('#coinDisplay');
+const storageButton1 = document.querySelector('#tree-storage');
+const storageButton2 = document.querySelector('#rock-storage');
+const storageButton3 = document.querySelector('#house-storage');
+const storageButton4 = document.querySelector('#flower-storage');
+const storageButton5 = document.querySelector('#path-storage');
+const treeStorageCount = document.querySelector('#tree-storage .cost');
+const rockStorageCount = document.querySelector('#rock-storage .cost');
+const houseStorageCount = document.querySelector('#house-storage .cost');
+const flowerStorageCount = document.querySelector('#flower-storage .cost');
+const pathStorageCount = document.querySelector('#path-storage .cost');
+let key1 = 'tree-item';
+let key2 = 'rock-item';
+let key3 = 'house-item';
+let key4 = 'flower-item';
+let key5 = 'path-item';
+treeStorageCount.textContent = 'x' + (window.purchasedItems[key1] || 0);
+rockStorageCount.textContent = 'x' + (window.purchasedItems[key2] || 0);
+houseStorageCount.textContent = 'x' + (window.purchasedItems[key3] || 0);
+flowerStorageCount.textContent = 'x' + (window.purchasedItems[key4] || 0);
+pathStorageCount.textContent = 'x' + (window.purchasedItems[key5] || 0);
 
 import anime from 'animejs/lib/anime.es.js';
+coinButton.textContent = window.coins;
+
+// coinButton.addEventListener('click', function(){
+//     coinButton.textContent(window.coins);
+// })
+
 
 shopButton1.addEventListener('click', function(){
     if (window.coins >= 10) {
         window.coins -= 10;
         console.log("purchased");
+        window.purchasedItems[key1] = (window.purchasedItems[key1] || 0) + 1; // Increment the count
         console.log(window.coins);
-        window.purchasedItems['tree-item'] = (window.purchasedItems['tree-item'] || 0) + 1; // Increment the count
         console.log(window.purchasedItems);
+        coinButton.textContent = window.coins;
+        treeStorageCount.textContent = 'x' + window.purchasedItems[key1];
+
     }
     else {
         alert('You do not have enough coins');
@@ -34,9 +64,12 @@ shopButton2.addEventListener('click', function(){
     if (window.coins >= 10) {
         window.coins -= 10;
         console.log("purchased");
+        window.purchasedItems[key2] = (window.purchasedItems[key2] || 0) + 1; // Increment the count
         console.log(window.coins);
-        window.purchasedItems['rock-item'] = (window.purchasedItems['rock-item'] || 0) + 1; // Increment the count
         console.log(window.purchasedItems);
+        coinButton.textContent = window.coins;
+        rockStorageCount.textContent = 'x' + window.purchasedItems[key2];
+
         
     }
     else {
@@ -47,9 +80,12 @@ shopButton3.addEventListener('click', function(){
     if (window.coins >= 10) {
         window.coins -= 10;
         console.log("purchased");
+        window.purchasedItems[key3] = (window.purchasedItems[key3] || 0) + 1; // Increment the count
         console.log(window.coins);
-        window.purchasedItems['house-item'] = (window.purchasedItems['house-item'] || 0) + 1; // Increment the count
         console.log(window.purchasedItems);
+        coinButton.textContent = window.coins;
+        houseStorageCount.textContent = 'x' + window.purchasedItems[key3];
+
     }
     else {
         alert('You do not have enough coins');
@@ -59,9 +95,12 @@ shopButton4.addEventListener('click', function(){
     if (window.coins >= 10) {
         window.coins -= 10;
         console.log("purchased");
+        window.purchasedItems[key4] = (window.purchasedItems[key4] || 0) + 1; // Increment the count
         console.log(window.coins);
-        window.purchasedItems['flower-item'] = (window.purchasedItems['flower-item'] || 0) + 1; // Increment the count
         console.log(window.purchasedItems);
+        coinButton.textContent = window.coins;
+        flowerStorageCount.textContent = 'x' + window.purchasedItems[key4];
+
     }
     else {
         alert('You do not have enough coins');
@@ -71,9 +110,12 @@ shopButton5.addEventListener('click', function(){
     if (window.coins >= 10) {
         window.coins -= 10;
         console.log("purchased");
+        window.purchasedItems[key5] = (window.purchasedItems[key5] || 0) + 1; // Increment the count
         console.log(window.coins);
-        window.purchasedItems['path-item'] = (window.purchasedItems['path-item'] || 0) + 1; // Increment the count
         console.log(window.purchasedItems);
+        coinButton.textContent = window.coins;
+        pathStorageCount.textContent = 'x' + window.purchasedItems[key5];
+
     }
     else {
         alert('You do not have enough coins');
@@ -101,7 +143,9 @@ settingsButton.addEventListener('click', function(){
  shopMainButton.addEventListener('click', function(){
     if(shopContainer.style.opacity != "1"){
         shopContainer.style.opacity = "1";
+        shopContainer.style.zIndex = "10";
         storageContainer.style.opacity = "0";
+        storageContainer.style.zIndex = "-10";
         window.initializedShop = true;
         
         anime ({
@@ -112,6 +156,7 @@ settingsButton.addEventListener('click', function(){
          });
 
    }else { shopContainer.style.opacity = "0";
+        shopContainer.style.zIndex = "-10";
         window.initializedShop = true;
         anime ({
             targets: timeFrame,
@@ -126,8 +171,10 @@ settingsButton.addEventListener('click', function(){
  storageMainButton.addEventListener('click', function(){
     if(storageContainer.style.opacity != "1"){
         storageContainer.style.opacity = "1";
+        storageContainer.style.zIndex = "10";
         window.initializedShop = true;
         shopContainer.style.opacity = "0";
+        shopContainer.style.zIndex = "-10";
         anime ({
             targets: timeFrame,
             opacity: 0,
@@ -137,6 +184,7 @@ settingsButton.addEventListener('click', function(){
 
    }else { 
         storageContainer.style.opacity = "0";
+        storageContainer.style.zIndex = "-10";
         window.initializedShop = false;
         anime ({
             targets: timeFrame,
@@ -150,4 +198,63 @@ settingsButton.addEventListener('click', function(){
     });
 
 
- 
+storageButton1.addEventListener('click', function(){
+    if (window.purchasedItems.hasOwnProperty(key1) && window.purchasedItems[key1] > 0) {
+        window.purchasedItems[key1]--; // Decrement the count
+        console.log("object created");
+
+        treeStorageCount.textContent = 'x' + window.purchasedItems[key1];
+
+    }
+    else {console.log("You do not have sufficient objects");
+}
+    
+});
+storageButton2.addEventListener('click', function(){
+    if (window.purchasedItems.hasOwnProperty(key2) && window.purchasedItems[key2] > 0) {
+        window.purchasedItems[key2]--; // Decrement the count
+        console.log("object created");
+
+        rockStorageCount.textContent = 'x' + window.purchasedItems[key2];
+
+    }
+    else {console.log("You do not have sufficient objects");
+}
+    
+});
+storageButton3.addEventListener('click', function(){
+    if (window.purchasedItems.hasOwnProperty(key3) && window.purchasedItems[key3] > 0) {
+        window.purchasedItems[key3]--; // Decrement the count
+        console.log("object created");
+
+        houseStorageCount.textContent = 'x' + window.purchasedItems[key3];
+
+    }
+    else {console.log("You do not have sufficient objects");
+}
+    
+});
+storageButton4.addEventListener('click', function(){
+    if (window.purchasedItems.hasOwnProperty(key4) && window.purchasedItems[key4] > 0) {
+        window.purchasedItems[key4]--; // Decrement the count
+        console.log("object created");
+
+        flowerStorageCount.textContent = 'x' + window.purchasedItems[key4];
+
+    }
+    else {console.log("You do not have sufficient objects");
+}
+    
+});
+storageButton5.addEventListener('click', function(){
+    if (window.purchasedItems.hasOwnProperty(key5) && window.purchasedItems[key5] > 0) {
+        window.purchasedItems[key5]--; // Decrement the count
+        console.log("object created");
+
+        pathStorageCount.textContent = 'x' + window.purchasedItems[key5];
+
+    }
+    else {console.log("You do not have sufficient objects");
+}
+    
+});
