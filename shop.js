0734edea944156faa3f90d90969cfdfab336e150
statkins <statkins@uiowa.edu>
2024-02-24 21:33:54 -0600

    shop changes + index script

--- shop.js
+++ shop.js
@@ -1,59 +1,63 @@
-window.coins += 10; 
-const shopButton1 = document.querySelector('#setButton');
-const shopButton2 = document.querySelector('#setButton');
-const shopButton3 = document.querySelector('#setButton');
-const shopButton4 = document.querySelector('#setButton');
-const shopButton5 = document.querySelector('#setButton');
+window.coins += 100; 
+console.log(window.coins);
+const shopButton1 = document.querySelector('#tree-item');
+const shopButton2 = document.querySelector('#rock-item');
+const shopButton3 = document.querySelector('#house-item');
+const shopButton4 = document.querySelector('#flower-item');
+const shopButton5 = document.querySelector('#path-item');
+
+
+
 
 shopButton1.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
     }
 });
 shopButton2.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
     }
 });
 shopButton3.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
     }
 });
 shopButton4.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
     }
 });
 shopButton5.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
     }
 });
 shopButton1.addEventListener('click', function(){
-    if (coins >= 10) {
-        coins -= 10;
-        shopButton1.textContent = 'Purchased';
+    if (window.coins >= 10) {
+        window.coins -= 10;
+        console.log("purchased");
     }
     else {
         alert('You do not have enough coins');
